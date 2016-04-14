edc9499ac5aa061c5ce7d2da9f80245deee6703b
Peter Halliday <pghalliday@gmail.com>
2016-04-14 14:24:47 +0200

    i think ~ doesn't work inside quotes or something

--- shared.sh
+++ shared.sh
@@ -6,16 +6,18 @@ fi
 
 function clone_or_pull {
   repo=$1
-  echo "setting up $repo"
   if [ ! -d ~/$repo ]; then
+    echo "cloning $repo"
     cd ~
     git clone --recursive ${GIT_PREFIX}pghalliday-dotfiles/${repo}.git
   else
+    echo "updating $repo"
     cd ~/$repo
     git pull && git submodule init && git submodule update && git submodule status
   fi
-  if [ -d "~/$repo/dotfiles" ]; then
+  if [ -d ~/$repo/dotfiles ]; then
+    echo "copying dotfiles into place"
     shopt -s dotglob
-    cp -rf ~/$repo/dotfiles/* ~
+    cp -rvf ~/$repo/dotfiles/* ~
   fi
 }
